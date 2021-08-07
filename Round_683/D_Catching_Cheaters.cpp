cd25ecec8a33cd9948ddbb59c3d32a673a5597b0
kushagra1212 <kushagrarathore002@gmail.com>
2021-08-07 20:11:19 +0530

    Q D

---
+++ Round_683/D_Catching_Cheaters.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long int
+#define nline "\n"
+#define FAST ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define deb(a) cerr<<#a<<" "<<a<<nline
+#define all(a) a.begin(),a.end()
+#define ctoi(c) (int)((int)c-'0')
+#define debv(v) cerr<<#v<<" => [ "; for(auto &vv:v){cerr<<vv<<" ";}cerr<<"]"<<nline
+#define deba(a,k,n) cerr<<#a<<" => [ "; for(int i=k;i<n;i++){cerr<<a[i]<<" ";}cerr<<"]"<<nline
+#define loop(i,s,e) for(int i=s;i<e;i++)
+#define itoc(i) (char)(i+'0')
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+private:
+public:
+ 
+void solve(){
+    int n,m; cin>>n>>m;
+
+    string s,t; cin>>s>>t;
+
+    int dp[n+1][m+1];
+    memset(dp,0,sizeof(dp));
+    int ans=INT64_MIN;
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=m;j++)
+        {
+            if(i==0 || j==0)
+             dp[i][j]=0;
+            else if(s[i-1]==t[j-1])
+            {
+                dp[i][j]=max(dp[i][j],dp[i-1][j-1]+2);
+            }else{
+                dp[i][j]=max(dp[i][j],max(dp[i-1][j]-1,dp[i][j-1]-1));
+            }
+          ans=max(ans,dp[i][j]);
+        }
+      
+    }
+    cout<<ans<<nline;
+
+  }
+};
+
+int32_t main()
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+  FAST; Solution sol;
+  // #ifndef ONLINE_JUDGE
+  //  freopen("input.txt","r",stdin);
+  //  freopen("output.txt","w", stdout);
+  // #endif
+  int t;
+  t = 1; 
+  //cin >> t;
+  while (t--)
+    sol.solve();
+
+ // auto end = std::chrono::high_resolution_clock::now();
+ // auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+ // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+
+  return 0;
+}
