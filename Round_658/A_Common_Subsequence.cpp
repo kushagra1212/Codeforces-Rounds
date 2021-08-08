68463a04988ddbf58eca3da08339ca0da5f9df1f
kushagra1212 <kushagrarathore002@gmail.com>
2021-08-08 15:37:38 +0530

    Q A

---
+++ Round_658/A_Common_Subsequence.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long int
+#define nline "\n"
+#define FAST ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define deb(a) cerr<<#a<<" "<<a<<nline
+#define all(a) a.begin(),a.end()
+#define ctoi(c) (int)((int)c-'0')
+#define debv(v) cerr<<#v<<" => [ "; for(auto &vv:v){cerr<<vv<<" ";}cerr<<"]"<<nline
+#define deba(a,k,n) cerr<<#a<<" => [ "; for(int i=k;i<n;i++){cerr<<a[i]<<" ";}cerr<<"]"<<nline
+#define loop(i,s,e) for(int i=s;i<e;i++)
+#define itoc(i) (char)(i+'0')
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+private:
+public:
+ 
+void solve(){
+   
+  int n,m; cin>>n>>m;
+vector<int> a(n);
+ vector<int> b(m);
+  for(int i=0;i<n;i++)
+        cin>>a[i];
+  
+  for(int i=0;i<m;i++)
+        cin>>b[i];
+  
+  sort(all(b));
+  int ans=INT64_MAX;
+  for(int i=0;i<n;i++)
+  {
+          auto it=lower_bound(all(b),a[i]);
+          if(*it==a[i])
+          {
+                  ans=*it;
+          }
+  
+  } 
+      if(ans!=INT64_MAX)
+        {
+                cout<<"YES\n";
+        cout<<1<<" "<<ans<<nline;
+        }else{
+                cout<<"NO\n";
+        }
+
+  }
+};
+
+int32_t main()
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+  FAST; Solution sol;
+  // #ifndef ONLINE_JUDGE
+  //  freopen("input.txt","r",stdin);
+  //  freopen("output.txt","w", stdout);
+  // #endif
+  int t;
+  t = 1; 
+  cin >> t;
+  while (t--)
+    sol.solve();
+
+ // auto end = std::chrono::high_resolution_clock::now();
+ // auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+ // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+
+  return 0;
+}
