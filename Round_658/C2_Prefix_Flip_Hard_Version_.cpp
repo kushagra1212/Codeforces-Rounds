ac452c6a0abbb471a06ba1d1a0a9d2f3b1737300
kushagra1212 <kushagrarathore002@gmail.com>
2021-08-08 15:38:40 +0530

    :Q C2

---
+++ Round_658/C2_Prefix_Flip_Hard_Version_.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long int
+#define nline "\n"
+#define FAST ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define deb(a) cerr<<#a<<" "<<a<<nline
+#define all(a) a.begin(),a.end()
+#define ctoi(c) (int)((int)c-'0')
+#define debv(v) cerr<<#v<<" => [ "; for(auto &vv:v){cerr<<vv<<" ";}cerr<<"]"<<nline
+#define deba(a,k,n) cerr<<#a<<" => [ "; for(int i=k;i<n;i++){cerr<<a[i]<<" ";}cerr<<"]"<<nline
+#define loop(i,s,e) for(int i=s;i<e;i++)
+#define itoc(i) (char)(i+'0')
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+private:
+public:
+ 
+void solve(){
+         int n;
+         string a,b;
+        cin >> n >> a >> b;
+        vector<int> ops1, ops2;
+        a += '0'; b += '0';
+        for(int i = 1; i <= n; i++) {
+            if(a[i] != a[i - 1]) ops1.push_back(i);
+            if(b[i] != b[i - 1]) ops2.push_back(i);
+        }
+        ops1.insert(ops1.end(), ops2.rbegin(), ops2.rend());
+        cout << ops1.size();
+        for(int x : ops1) {
+            cout << ' ' << x;
+        }
+        cout << '\n';
+
+
+
+  }
+};
+
+int32_t main()
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+  FAST; Solution sol;
+  // #ifndef ONLINE_JUDGE
+  //  freopen("input.txt","r",stdin);
+  //  freopen("output.txt","w", stdout);
+  // #endif
+  int t;
+  t = 1; 
+  cin >> t;
+  while (t--)
+    sol.solve();
+
+ // auto end = std::chrono::high_resolution_clock::now();
+ // auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+ // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+
+  return 0;
+}
