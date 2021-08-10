1afcec166daa5e5ea0a9c882140fa4bdd6730a97
kushagra1212 <kushagrarathore002@gmail.com>
2021-08-10 21:53:04 +0530

    Q B

---
+++ Practice/B_Find_The_Array.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long int
+#define nline "\n"
+#define FAST ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define deb(a) cerr<<#a<<" "<<a<<nline
+#define all(a) a.begin(),a.end()
+#define ctoi(c) (int)((int)c-'0')
+#define debv(v) cerr<<#v<<" => [ "; for(auto &vv:v){cerr<<vv<<" ";}cerr<<"]"<<nline
+#define deba(a,k,n) cerr<<#a<<" => [ "; for(int i=k;i<n;i++){cerr<<a[i]<<" ";}cerr<<"]"<<nline
+#define loop(i,s,e) for(int i=s;i<e;i++)
+#define itoc(i) (char)(i+'0')
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+private:
+public:
+ 
+void solve(){
+   
+   int n; cin>>n;
+   int a[n];
+   int sum[2]={0,0};
+    int s=0;
+   for(int i=0;i<n;i++)
+   {
+           cin>>a[i];
+           s+=a[i];
+           sum[i%2]+=a[i]-1;
+   }
+   for(int i=0;i<2;i++)
+   {
+           if(2*sum[i]>s)
+             continue;
+         
+                   for(int j=0;j<n;j++)
+                   {
+                           if(j%2==i)
+                             a[j]=1;
+                   }
+         break;
+
+   }
+   for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+        cout<<nline;
+
+
+  }
+};
+
+int32_t main()
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+  FAST; Solution sol;
+  // #ifndef ONLINE_JUDGE
+  //  freopen("input.txt","r",stdin);
+  //  freopen("output.txt","w", stdout);
+  // #endif
+  int t;
+  t = 1; 
+  cin >> t;
+  while (t--)
+    sol.solve();
+
+ // auto end = std::chrono::high_resolution_clock::now();
+ // auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+ // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+
+  return 0;
+}
