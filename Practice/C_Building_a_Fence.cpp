0b7dabb89b9a1fa399ddaade62e2f36f2c86078e
kushagra1212 <kushagrarathore002@gmail.com>
2021-08-13 22:02:08 +0530

    Q C

---
+++ Practice/C_Building_a_Fence.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long int
+#define nline "\n"
+#define FAST ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define deb(a) cerr<<#a<<" "<<a<<nline
+#define all(a) a.begin(),a.end()
+#define ctoi(c) (int)((int)c-'0')
+#define debv(v) cerr<<#v<<" => [ "; for(auto &vv:v){cerr<<vv<<" ";}cerr<<"]"<<nline
+#define deba(a,k,n) cerr<<#a<<" => [ "; for(int i=k;i<n;i++){cerr<<a[i]<<" ";}cerr<<"]"<<nline
+#define loop(i,s,e) for(int i=s;i<e;i++)
+#define itoc(i) (char)(i+'0')
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+private:
+public:
+ 
+void solve(){
+   
+   int n,k; cin>>n>>k;
+   int h[n];
+   for(int i=0;i<n;i++){
+           cin>>h[i];
+   }
+   
+   vector<pair<int,int>> dp(n);
+   dp[0]={h[0],h[0]};
+   for(int i=1;i<n;i++){
+         
+         dp[i].first=max(h[i],dp[i-1].first-k+1);
+         dp[i].second=min(h[i]+k-1,dp[i-1].second+k-1);
+        if(dp[i].first>dp[i].second)
+        {
+                cout<<"NO\n";
+                return;
+        }
+   }
+   if(dp[n-1].first>h[n-1]){
+           cout<<"NO\n";
+           return;
+   }
+   cout<<"YES\n";
+   return;
+
+
+  }
+};
+
+int32_t main()
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+  FAST; Solution sol;
+  // #ifndef ONLINE_JUDGE
+  //  freopen("input.txt","r",stdin);
+  //  freopen("output.txt","w", stdout);
+  // #endif
+  int t;
+  t = 1; 
+cin >> t;
+  while (t--)
+    sol.solve();
+
+ // auto end = std::chrono::high_resolution_clock::now();
+ // auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+ // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+
+  return 0;
+}
