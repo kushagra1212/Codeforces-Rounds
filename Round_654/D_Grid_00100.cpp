a2551165114fb4b87d41f300854641d85adcb6b7
kushagra1212 <kushagrarathore002@gmail.com>
2021-08-21 16:24:30 +0530

    Q D

---
+++ Round_654/D_Grid_00100.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long int
+#define nline "\n"
+#define FAST ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define deb(a) cerr<<#a<<" "<<a<<nline
+#define all(a) a.begin(),a.end()
+#define ctoi(c) (int)((int)c-'0')
+#define debv(v) cerr<<#v<<" => [ "; for(auto &vv:v){cerr<<vv<<" ";}cerr<<"]"<<nline
+#define deba(a,k,n) cerr<<#a<<" => [ "; for(int i=k;i<n;i++){cerr<<a[i]<<" ";}cerr<<"]"<<nline
+#define loop(i,s,e) for(int i=s;i<e;i++)
+#define itoc(i) (char)(i+'0')
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+private:
+public:
+ 
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    int a[n][n];
+        if(k%n==0){
+            cout<<0<<nline;
+    }else
+     cout<<2<<nline;
+    memset(a,0,sizeof(a));
+    for(int i=0;i<n and k;i++){
+            for(int j=0;j<n and k;j++){
+                    int x=j;
+                    int y=(i+j)%n;
+                    a[x][y]=1;
+                    k--;
+            }
+    }
+
+    for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                    cout<<a[i][j];
+            }
+    cout<<nline;
+    }
+    
+
+
+
+  }
+};
+
+int32_t main()
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+  FAST; Solution sol;
+  // #ifndef ONLINE_JUDGE
+  //  freopen("input.txt","r",stdin);
+  //  freopen("output.txt","w", stdout);
+  // #endif
+  int t;
+  t = 1; 
+  cin >> t;
+  while (t--)
+    sol.solve();
+
+ // auto end = std::chrono::high_resolution_clock::now();
+ // auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+ // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+
+  return 0;
+}
