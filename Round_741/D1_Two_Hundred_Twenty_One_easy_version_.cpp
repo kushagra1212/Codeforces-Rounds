8dca7993f3e19b5afd74a5c3f7adaea28258c1f5
kushagra1212 <kushagrarathore002@gmail.com>
2021-08-27 18:51:53 +0530

    Q D

---
+++ Round_741/D1_Two_Hundred_Twenty_One_easy_version_.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long int
+#define nline "\n"
+#define FAST ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define deb(a) cerr<<#a<<" "<<a<<nline
+#define all(a) a.begin(),a.end()
+#define ctoi(c) (int)((int)c-'0')
+#define debv(v) cerr<<#v<<" => [ "; for(auto &vv:v){cerr<<vv<<" ";}cerr<<"]"<<nline
+#define deba(a,k,n) cerr<<#a<<" => [ "; for(int i=k;i<n;i++){cerr<<a[i]<<" ";}cerr<<"]"<<nline
+#define loop(i,s,e) for(int i=s;i<e;i++)
+#define itoc(i) (char)(i+'0')
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+private:
+public:
+ 
+void solve(){
+      int n,q; cin>>n>>q;
+     string s; cin>>s;
+     int a[n];
+     for(int  i=0;i<n;i++){
+            if(i%2==0){
+                    if(s[i]=='+')
+                      a[i]=1;
+                    else
+                        a[i]=-1;
+            }else{
+                      if(s[i]=='+')
+                      a[i]=-1;
+                    else
+                        a[i]=1; 
+            }
+             
+     }
+     int prefix[n+1];
+     prefix[0]=0;
+     prefix[1]=a[0];
+     for(int i=2;i<=n;i++){
+         prefix[i]=prefix[i-1]+a[i-1];
+     }
+     while(q--){
+             int x,y; cin>>x>>y;
+             int ans=prefix[y]-prefix[x-1];
+             if(ans!=0){
+                     if(ans%2)
+                       cout<<1<<nline;
+                     else
+                       cout<<2<<nline;
+             }else
+               cout<<ans<<nline;
+     }
+
+
+
+  }
+};
+
+int32_t main()
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+  FAST; Solution sol;
+  // #ifndef ONLINE_JUDGE
+  //  freopen("input.txt","r",stdin);
+  //  freopen("output.txt","w", stdout);
+  // #endif
+  int t;
+  t = 1; 
+  cin >> t;
+  while (t--)
+    sol.solve();
+
+ // auto end = std::chrono::high_resolution_clock::now();
+ // auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+ // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+
+  return 0;
+}
