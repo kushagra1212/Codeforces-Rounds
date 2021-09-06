ea46c8016b72c516fa455b9f1acf2b2d5b0f5ec9
kushagra1212 <kushagrarathore002@gmail.com>
2021-09-06 23:56:03 +0530

    Q A

---
+++ Practice/A.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long int
+#define nline "\n"
+#define FAST ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define deb(a) cerr<<#a<<" "<<a<<nline
+#define all(a) a.begin(),a.end()
+#define ctoi(c) (int)((int)c-'0')
+#define debv(v) cerr<<#v<<" => [ "; for(auto &vv:v){cerr<<vv<<" ";}cerr<<"]"<<nline
+#define deba(a,k,n) cerr<<#a<<" => [ "; for(int i=k;i<n;i++){cerr<<a[i]<<" ";}cerr<<"]"<<nline
+#define loop(i,s,e) for(int i=s;i<e;i++)
+#define itoc(i) (char)(i+'0')
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+private:
+public:
+ 
+void solve(){
+   
+    string s;
+    int n;
+    cin>>n;
+    cin>>s;
+    string ans="";
+
+    for(int i=0;i<n;i++){
+        if(s[i]=='U')
+          ans+='D';
+        else if(s[i]=='D')
+          ans+='U';
+        else if(s[i]=='L')
+          ans+='L';
+        else if(s[i]=='R')
+         ans+='R';
+     
+    }
+    cout<<ans<<nline;
+
+
+
+  }
+};
+
+int32_t main()
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+  FAST; Solution sol;
+  // #ifndef ONLINE_JUDGE
+  //  freopen("input.txt","r",stdin);
+  //  freopen("output.txt","w", stdout);
+  // #endif
+  int t;
+  t = 1; 
+  cin >> t;
+  while (t--)
+    sol.solve();
+
+ // auto end = std::chrono::high_resolution_clock::now();
+ // auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+ // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+
+  return 0;
+}
