1b79df0763c51be12ecce56a7de19b1f2bff39d1
kushagra1212 <kushagrarathore002@gmail.com>
2021-09-12 23:57:51 +0530

    Q D

---
+++ Practice/D_Zookeeper_and_The_Infinite_Zoo.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long int
+#define nline "\n"
+#define FAST ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define deb(a) cerr<<#a<<" "<<a<<nline
+#define all(a) a.begin(),a.end()
+#define ctoi(c) (int)((int)c-'0')
+#define debv(v) cerr<<#v<<" => [ "; for(auto &vv:v){cerr<<vv<<" ";}cerr<<"]"<<nline
+#define deba(a,k,n) cerr<<#a<<" => [ "; for(int i=k;i<n;i++){cerr<<a[i]<<" ";}cerr<<"]"<<nline
+#define loop(i,s,e) for(int i=s;i<e;i++)
+#define itoc(i) (char)(i+'0')
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+private:
+public:
+
+ 
+void solve()     {
+     
+     int x,y;
+     cin>>x>>y;
+    
+    if(x>y){
+            cout<<"NO\n";
+            return ;
+    }
+    if(x==y){
+            cout<<"YES\n";
+            return;
+    }
+    bool isPossible=true;
+    int a1=0,b1=0;
+    while(y>0){
+        a1+=x%2;
+        b1+=y%2;
+        x>>=1;
+        y>>=1;
+        if(a1<b1){
+                isPossible=false;
+                break;
+        }
+    }
+    if(isPossible)
+    cout<<"YES\n";
+    else
+    cout<<"NO\n";
+     
+     
+
+
+
+  }
+};
+
+int32_t main()
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+  FAST; Solution sol;
+  // #ifndef ONLINE_JUDGE
+  //  freopen("input.txt","r",stdin);
+  //  freopen("output.txt","w", stdout);
+  // #endif
+  int t;
+  t = 1; 
+  cin >> t;
+  while (t--)
+    sol.solve();
+
+ // auto end = std::chrono::high_resolution_clock::now();
+ // auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+ // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+
+  return 0;
+}
