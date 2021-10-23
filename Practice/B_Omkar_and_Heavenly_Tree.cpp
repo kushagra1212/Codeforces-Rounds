c5571891e8e6b267ab9278c6c2fb546ca86ccf14
kushagra1212 <kushagrarathore002@gmail.com>
2021-10-23 14:55:41 +0530

    Q B

---
+++ Practice/B_Omkar_and_Heavenly_Tree.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long int
+#define nline "\n"
+#define FAST ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define deb(a) cerr<<#a<<" "<<a<<nline
+#define all(a) a.begin(),a.end()
+#define ctoi(c) (int)((int)c-'0')
+#define debv(v) cerr<<#v<<" => [ "; for(auto &vv:v){cerr<<vv<<" ";}cerr<<"]"<<nline
+#define deba(a,k,n) cerr<<#a<<" => [ "; for(int i=k;i<n;i++){cerr<<a[i]<<" ";}cerr<<"]"<<nline
+#define loop(i,s,e) for(int i=s;i<e;i++)
+#define itoc(i) (char)(i+'0')
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+private:
+public:
+ 
+void solve(){
+   
+    int n,m; cin>>n>>m;
+    int a,b,c;
+    map<int,int> mp;
+    
+    for(int i=0;i<m;i++){
+        cin>>a>>b>>c;
+  
+        mp[b]=1;;
+    }
+
+    int root;
+    for(int i=1;i<=n;i++){
+        if(mp[i]==0){
+            root=i;
+            break;
+        }
+    }
+    for(int i=1;i<=n;i++){
+        if(i==root)
+            continue;
+        cout<<i<<" "<<root<<nline;
+    }
+
+
+  }
+};
+
+int32_t main()
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+  FAST; Solution sol;
+  // #ifndef ONLINE_JUDGE
+  //  freopen("input.txt","r",stdin);
+  //  freopen("output.txt","w", stdout);
+  // #endif
+  int t;
+  t = 1; 
+  cin >> t;
+  while (t--)
+    sol.solve();
+
+  auto end = std::chrono::high_resolution_clock::now();
+  auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+
+  return 0;
+}
